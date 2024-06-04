c194e4099d4c895a18b41a0394c30b7b5223b98f
yoonwooiOS <yoonwoo4429@kakao.com>
2024-06-05 01:04:00 +0900

    Enum 타입으로 코드 리팩토링

--- TableViewExample/Setting/SettingTableViewController.swift
+++ TableViewExample/Setting/SettingTableViewController.swift
@@ -6,84 +6,82 @@
 //
 
 import UIKit
+enum SettingOptions: Int, CaseIterable {
+    
+    case total, personal, others
+    
+    var mainOptions: String {
+        switch self {
+        case .total:
+            return "전체 설정"
+        case .personal:
+            return "개인 설정"
+        case .others:
+            return "기타"
+        }
+    }
+    
+    var subOptions: [String] {
+        switch self {
+        case .total:
+            return ["공지사항", "실험실", "버전 정보"]
+        case .personal:
+            return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .others:
+            return ["전체 설정", "개인 설정", "기타"]
+        }
+    }
+}
+
 
 class SettingTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        configureTableView()
     }
-
-    // MARK: - Table view data source
-
+    
+    // 섹션 개수
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return SettingOptions.allCases.count
     }
+    
+    // Header 설정
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
 
+        if section == SettingOptions.total.rawValue {
+            return SettingOptions.total.mainOptions
+        } else if section == SettingOptions.personal.rawValue {
+            return SettingOptions.personal.mainOptions
+        } else {
+            return SettingOptions.others.mainOptions
+        }
+        
+    }
+    
+    // 각 세션에 들어가는 셀의 개수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return SettingOptions.allCases[section].subOptions.count
     }
-
-    /*
+    
+    
+    
+    
+    
+    // 각 셀 구현
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell")!
 
-        // Configure the cell...
-
+        cell.textLabel?.text = SettingOptions.allCases[indexPath.section].subOptions[indexPath.row]
+    
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func configureTableView() {
+        
+        tableView.rowHeight = 44
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
